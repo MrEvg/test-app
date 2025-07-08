5b1f622ebc34cfbd0c108c457dca6225dc9c20c2
MrEvg <evgenyakubovskiy@yandex.ru>
2025-07-08 15:06:55 +0300

    feat(core): added fsd

---
+++ src/app/App.tsx
@@ -0,0 +1,31 @@
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text style={styles.text}>Test app</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+});
+
+export default App;
